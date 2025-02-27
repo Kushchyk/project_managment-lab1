1e64231ac1613ea0d4e0091cea6abf5ca0af739a
Kushchyk Andrii <andrii.kushchyk.pp.2022@lpnu.ua>
2025-02-27 10:04:41 +0200

    set up file for Tests

--- src/test/java/com/ti/main/MainApplicationTests.java
+++ src/test/java/com/ti/main/MainApplicationTests.java
@@ -3,7 +3,7 @@ package com.ti.main;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.ti.main.sessiongame.MainApplication.class)
 class MainApplicationTests {
 
     @Test
